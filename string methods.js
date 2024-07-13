5826e30f16f26c50ef77074da78ae7f7c1a5388d
Yubesh <yubeshbhattarai@gmail.com>
2024-07-13 23:22:34 +0545

    java scripts programs

---
+++ string methods.js
@@ -0,0 +1,19 @@
+//changing into Uppercase
+let name="kathmandu";
+console.log(name.length);
+console.log(name.slice(0,6));
+console.log(name.toUpperCase());
+console.log(name.replace("kathmandu", "New road"));
+
+//changing into Lowercase
+let god="SHIVA ";
+console.log(god.length);
+console.log(god.slice(0,3));
+console.log(god.toLowerCase());
+console.log(god.slice(2));
+console.log(god.replace("SHI", "BIS"));
+
+
+// //string concationation
+console.log(god.concat( " is the ruler of " , name));
+
